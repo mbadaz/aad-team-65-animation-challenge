cd96cfcb445758a714fcd36dc2e096be1af06605
tapsmuzira@gmail.com <tapsmuzira@gmail.com>
2019-10-15 19:59:48 +0200

    Initial empty commit

---
+++ app/src/main/java/com/mambure/aad_team_65_animation_challenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mambure.aad_team_65_animation_challenge;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
