1185a008f6bea3eab79f6f20211a5f039c6ea40a
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-23 17:40:13 +0200

    shared element logic

--- app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/activities/SharedElementListActivity.kt
+++ app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/activities/SharedElementListActivity.kt
@@ -1,13 +1,41 @@
 package com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.activities
 
+import android.annotation.TargetApi
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import com.mambure.aad_team_65_animation_challenge.R
+import com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.adapters.ElementsRecyclerViewAdapter
+import com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.listeners.ElementClickListener
+import com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.model.ElementModel
+import kotlinx.android.synthetic.main.activity_shared_element_list.*
 
-class SharedElementListActivity : AppCompatActivity() {
+class SharedElementListActivity : AppCompatActivity(), ElementClickListener {
+
+    companion object {
+        const val TITLE = "title"
+        const val IMAGE = "image"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_element_list)
+
+        recyclerview.adapter = ElementsRecyclerViewAdapter(this)
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
+    }
+
+    @TargetApi(21)
+    override fun onElementClick(view: ImageView, element: ElementModel) {
+        //Open detail activity with shared element transition
+        val activityOptions = ActivityOptionsCompat
+                .makeSceneTransitionAnimation(this, view, "artwork")
+        val intent = Intent(this, SharedElementDetailActivity::class.java)
+        intent.putExtra(TITLE, element.title)
+        intent.putExtra(IMAGE, element.image)
+        startActivity(intent, activityOptions.toBundle())
     }
 }
--- app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/adapters/ElementsRecyclerViewAdapter.kt
+++ app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/adapters/ElementsRecyclerViewAdapter.kt
@@ -1,26 +1,52 @@
 package com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mambure.aad_team_65_animation_challenge.R
+import com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.listeners.ElementClickListener
+import com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.model.ElementModel
 
-class ElementsRecyclerViewAdapter() : RecyclerView.Adapter<ElementsRecyclerViewAdapter.ItemHolder>() {
+class ElementsRecyclerViewAdapter(private val listener: ElementClickListener)
+    : RecyclerView.Adapter<ElementsRecyclerViewAdapter.ItemHolder>() {
+
+    private val data = listOf(
+            ElementModel("Smoke + Mirrors", R.drawable.smoke),
+            ElementModel("Night  Visions", R.drawable.night),
+            ElementModel("Farewell Starlite!", R.drawable.cover)
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ItemHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_shared_element, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val element = data[position]
+        holder.bind(element)
+
+        holder.itemView.setOnClickListener {
+            listener.onElementClick(holder.image, element)
+        }
     }
 
     inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
+        internal val image: ImageView = itemView.findViewById(R.id.image)
+        private val title: TextView = itemView.findViewById(R.id.title)
 
+        fun bind(item: ElementModel) {
+            Glide.with(itemView.context)
+                    .load(item.image)
+                    .fitCenter()
+                    .into(image)
+
+            title.text = item.title
         }
     }
 }
