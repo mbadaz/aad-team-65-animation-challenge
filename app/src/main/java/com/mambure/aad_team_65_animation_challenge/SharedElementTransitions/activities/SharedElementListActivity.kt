87e6cb22cd9b3c478ec80f4f72383aa9c94f0345
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-23 16:18:17 +0200

    setup shared element controllers

---
+++ app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/activities/SharedElementListActivity.kt
@@ -0,0 +1,13 @@
+package com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mambure.aad_team_65_animation_challenge.R
+
+class SharedElementListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_element_list)
+    }
+}
---
+++ app/src/main/java/com/mambure/aad_team_65_animation_challenge/SharedElementTransitions/adapters/ElementsRecyclerViewAdapter.kt
@@ -0,0 +1,26 @@
+package com.mambure.aad_team_65_animation_challenge.SharedElementTransitions.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ElementsRecyclerViewAdapter() : RecyclerView.Adapter<ElementsRecyclerViewAdapter.ItemHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind() {
+
+        }
+    }
+}
